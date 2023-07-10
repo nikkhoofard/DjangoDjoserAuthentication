d9e138519c7838f823de95071e21630e7648623a
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-07-10 05:11:52 +0430

    configure database postgres

--- djoserauthapi/settings.py
+++ djoserauthapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*xl=p_2$t$q(60s7sez_d)&vww_h7t4fmb=j3_%v@f#i3w7_%+'
+SECRET_KEY = os.environ.get('SECURITY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
     'djoser',
 
     "corsheaders"
+
+    'account'
 
 
 
@@ -86,10 +89,15 @@ WSGI_APPLICATION = 'djoserauthapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
-}
+ }
+
 
 
 # Password validation
