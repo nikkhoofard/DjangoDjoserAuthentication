9cf1b0d71c9496ca1907c820db880886bc212f6c
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-07-10 08:12:54 +0430

    add djoser setting in DJOSER in setting.py
    cls

--- djoserauthapi/settings.py
+++ djoserauthapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 import os
 
 
@@ -166,9 +167,33 @@ REST_FRAMEWORK = {
 
 # jwt settings
 SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
+    'AUTH_HEADER_TYPES': ('JWT',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "UPDATE_LAST_LOGIN": False,
+
 }
 
+DJOSER = {
+    'LOGIN_FIELD' : 'email',
+    'USER_CREATE_PASSWORD_RETYPE' : True,
+    'ACTIVATION_URL' : '/activate/{uid}/{token}',
+    'SEND_CONFIRMATION_EMAIL' : True,
+    'SEND_CONFIRMATION_EMAIL' : True,
+    'PASSWORD_CHANGED_EMAIL_CONFIRMATION' : True,
+    'PASSWORD_RESET_CONFIRM_URL' : '/password-reset/{uid}/{token}',
+    'SET_PASSWORD_RETYPE'  : True,
+    'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND' : True,
+    'TOKEN_MODEL' : None,
+    'SERIALIZERS':{
+        'user_create': 'account.serializers.UserCreateSerializer',
+        'user': 'account.serializers.UserCreateSerializer',
+        'user_delete': 'djoser.serializers.UserDeleteSerializer',
+    }
+
+
+}
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
