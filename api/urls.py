520fd815a969b0822519dc6b67ee562115dab1f5
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-10-16 01:33:54 +0330

    add views serilizer and admin

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from . import views
+from .views import CreateAction, CreateUserAction
+from rest_framework.routers import DefaultRouter
+
+
+#router = DefaultRouter()
+#router.register("create/", CreateAction, basename='action')
+#router.register("create-user-action/", CreateUserAction, basename='user-action')
+urlpatterns = [
+    #path("", include(router.urls)),
+    path("create/", CreateAction.as_view)
+
+]
