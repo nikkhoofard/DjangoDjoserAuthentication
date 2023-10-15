6823ab256b3ae8e66b5fad51dcbdb07dfacbbe81
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-10-16 00:06:47 +0330

    add serializer file

---
+++ api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import UserAction, Action
+from django.contrib.auth import get_user_model
+from account.models import User
+
+
+class UserActionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAction
+        fields = "__all__"
+
+
+class ActionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Action
+        fields = "__all__"
